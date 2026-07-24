29cbb5f279ad6dca203201264d3f66bfbeb2c82b
NSNihalreddy <vtu28683@veltech.edu.in>
2026-07-24 09:23:30 +0530

    Initial commit

---
+++ task2.java
@@ -0,0 +1,13 @@
+import java.util.stream.IntStream;
+
+public class task2 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int index = 2;
+
+        IntStream.range(0, arr.length)
+                 .filter(i -> i == index)
+                 .map(i -> arr[i])
+                 .forEach(System.out::println);
+    }
+}
